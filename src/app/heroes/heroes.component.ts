609fd9a7fa923f93bd393ffff74ea6cf88232998
ViataF <viataruth@gmail.com>
2021-04-03 17:30:58 +0200

    feat(heroes component): add heroes + hero property

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero-models/hero';
+import { HEROES } from '../hero-models/mock-heroes';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+  
+  public heroes = HEROES;
+  public hero: Hero = {
+    id: 1,
+    name: "Windstorm"
+  } ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+``

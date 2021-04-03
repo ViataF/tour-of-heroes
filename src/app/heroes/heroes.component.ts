46e8338a036c1dcdd3abf1a79ef9de5d4eb0fd83
ViataF <viataruth@gmail.com>
2021-04-03 22:22:09 +0200

    feat( heroes component): inject the hero service + subscribe to getHeroes method + assigning value to hero method

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero-models/hero';
-import { HEROES } from '../hero-models/mock-heroes';
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  
-  public heroes = HEROES;
+  public heroes: Hero[] = [];
   public hero: Hero = {
     id: 1,
     name: "Windstorm"
   };
-  selectedHero?:Hero;
-  onSelect(hero: Hero): void {
+  public selectedHero?:Hero;
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  public onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  private getHeroes(): void{
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 
 }

d93e8b45e80615f180e15706a83be20cd03845a4
ViataF <viataruth@gmail.com>
2021-04-04 20:30:07 +0200

    feat(heroes component): add and delete subscription to obervables that adds and deletes hero from db

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,4 +22,18 @@ export class HeroesComponent implements OnInit {
         .subscribe(heroes => this.heroes = heroes);
   }
 
+  public add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+        .subscribe(hero => {
+          this.heroes.push(hero);
+        })
+  }
+
+  public delete(hero: Hero){
+    this.heroes = this.heroes.filter( dbhero => dbhero !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
 }
